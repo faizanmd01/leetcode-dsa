4c12b67f3e950876c9034b4609803d170e15ce4b
MD FAIZAN <98234991+faizanmd01@users.noreply.github.com>
2026-06-18 09:52:02 +0530

    Time: 3 ms (80.00%) | Memory: 20.5 MB (75.64%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,21 +1,13 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        # Do pointers set karein: ek shuruat mein aur ek aakhiri mein
-        left = 0
-        right = len(numbers) - 1
+        left, right = 0, len(numbers) - 1
         
         while left < right:
-            current_sum = numbers[left] + numbers[right]
-            
-            # Agar sum target ke barabar mil gaya
-            if current_sum == target:
-                # 1-indexed answer chahiye, isliye donon mein +1 karenge
+            s = numbers[left] + numbers[right]
+            if s == target:
                 return [left + 1, right + 1]
-            
-            # Agar sum target se bada hai, toh right pointer ko piche lao
-            elif current_sum > target:
-                right -= 1
-                
-            # Agar sum target se chota hai, toh left pointer ko aage badhao
-            else:
+            # Jo condition zyada sach hone ke chances hain use upar rakhein
+            if s < target:
                 left += 1
+            else:
+                right -= 1
